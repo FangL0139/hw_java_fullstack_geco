07bd7d1bd5032b26df081e0ccf6a8445894c2b44
Liu Fang <e0843571@u.nus.edu>
2022-10-07 01:25:54 +0800

    Update: 6 Oct Hw 20 - Java FileIO,DateTime

--- BackEnd_Java/JavaBasic_Git/src/W5_Hw/FileIO/FileOutput.java
+++ BackEnd_Java/JavaBasic_Git/src/W5_Hw/FileIO/FileOutput.java
@@ -14,8 +14,15 @@ public class FileOutput {
         User2 user2 = new User2("Anton-1", "Berlin");
         User2 user3 = new User2("Shinji-1", "Kyoto");
         User2 user4 = new User2("Au Yeong-2", "Hong Kong");
+        User2 user5 = new User2("Aaron-2","Boston");
+        User2 user6 = new User2("Nick-2","San Diego");
+        User2 user7 = new User2("Honda-3","Milan");
+        User2 user8 = new User2("Charlie-3","Sydney");
+        User2 user9 = new User2("Vivien-3","Hanoi");
+        User2 user10 = new User2("Peter-3","Rome");
 
-        ArrayList<User2> arr = new ArrayList<>(Arrays.asList(user1, user2, user3, user4));
+
+        ArrayList<User2> arr = new ArrayList<>(Arrays.asList(user1, user2, user3, user4,user5,user6,user7,user8,user9,user10));
 
         String filePath = "../Files/Users.txt";
         String timeFormat = "dd/MM/yyyy hh:mm:ss a";
